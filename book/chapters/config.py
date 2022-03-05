8682e7ac7bb9a17e7cb901ac37beb4e944ba7ad4
Gordon Watts <gordon@gordonandpaula.net>
2022-03-05 14:36:08 +0100

    Proof read of Jets and Calibration

--- book/chapters/config.py
+++ book/chapters/config.py
@@ -2,33 +2,11 @@ from dataclasses import dataclass
 from pathlib import Path
 
 from func_adl_servicex import SXLocalxAOD
-from func_adl_servicex_xaodr21 import SXDSAtlasxAODR21
+from func_adl_servicex_xaodr21 import SXDSAtlasxAODR21, calib_tools
 from func_adl_servicex_xaodr21.event_collection import Event
 
 import awkward as ak
 import numpy as np
-
-# rucio_zee_r21_mc = "rucio://mc15_13TeV:mc15_13TeV.361106.PowhegPythia8EvtGen_AZNLOCTEQ6L1_Zee.merge.DAOD_STDM3.e3601_s2576_s2132_r6630_r6264_p2363_tid05630052_00"
-
-# # Define the local dataset (per-machine), and datatype that uses it
-
-
-# local_xaod_path = Path(
-#     r"C:\Users\gordo\Code\atlas\data\xAODSampleFiles\DAOD_EXOT15.26710781._000001.pool.root.1"
-# )
-# if not local_xaod_path.exists():
-#     local_xaod_path = Path(
-#         r"C:\Users\gordo\Code\atlas\xAODSampleData\DAOD_EXOT15.26710781._000001.pool.root.1"
-#     )
-
-# # Define a dataset we can use so we can import this directly
-# # ds = SXDSAtlasxAODR21(rucio_zee_r21_mc, backend='dev_xaod')
-# ds = xAODLocalTyped(local_xaod_path)
-
-# # The following two lines will cause the servicex front end to ignore the cache
-# # from servicex import ignore_cache
-# # ic = ignore_cache()
-# # ic.__enter__()
 
 
 @dataclass
@@ -45,33 +23,45 @@ class sample:
 
 
 _samples = {
-    'zee': sample(
+    "zee": sample(
         name="ds_zee",
         rucio_ds="mc16_13TeV.361106.PowhegPythia8EvtGen_AZNLOCTEQ6L1_Zee.deriv.DAOD_PHYS.e3601_e5984_s3126_s3136_r10724_r10726_p4164",
-        local_path=Path(r"C:\Users\gordo\Code\atlas\data\R21\DAOD_PHYS\361106\DAOD_PHYS.23294912._000216.pool.root.1"),
+        local_path=Path(
+            r"C:\Users\gordo\Code\atlas\data\R21\DAOD_PHYS\361106\DAOD_PHYS.23294912._000216.pool.root.1"
+        ),
     ),
-    'zmumu': sample(
+    "zmumu": sample(
         name="ds_zmuumu",
         rucio_ds="mc16_13TeV.361107.PowhegPythia8EvtGen_AZNLOCTEQ6L1_Zmumu.deriv.DAOD_PHYS.e3601_e5984_s3126_s3136_r10724_r10726_p4164",
-        local_path=Path(r"C:\Users\gordo\Code\atlas\data\R21\DAOD_PHYS\361107\DAOD_PHYS.23295097._000229.pool.root.1"),
+        local_path=Path(
+            r"C:\Users\gordo\Code\atlas\data\R21\DAOD_PHYS\361107\DAOD_PHYS.23295097._000229.pool.root.1"
+        ),
     ),
-    'ztautau': sample(
+    "ztautau": sample(
         name="ds_ztautau",
         rucio_ds="mc16_13TeV.361108.PowhegPythia8EvtGen_AZNLOCTEQ6L1_Ztautau.deriv.DAOD_PHYS.e3601_e5984_s3126_s3136_r10724_r10726_p4164",
-        local_path=Path(r"C:\Users\gordo\Code\atlas\data\R21\DAOD_PHYS\361108\DAOD_PHYS.23295108._000430.pool.root.1"),
+        local_path=Path(
+            r"C:\Users\gordo\Code\atlas\data\R21\DAOD_PHYS\361108\DAOD_PHYS.23295108._000430.pool.root.1"
+        ),
     ),
-    'jz3_exot15': sample(
+    "jz2_exot15": sample(
         name="ds_jz3_exot15",
         rucio_ds="mc16_13TeV.361022.Pythia8EvtGen_A14NNPDF23LO_jetjet_JZ2W.deriv.DAOD_EXOT15.e3668_s3126_r9364_r9315_p4696",
-        local_path=Path(r"C:\Users\gordo\Code\atlas\data\R21\DAOD_PHYS\361022\DAOD_EXOT15.26710681._000512.pool.root.1"),
+        local_path=Path(
+            r"C:\Users\gordo\Code\atlas\data\R21\DAOD_PHYS\361022\DAOD_EXOT15.26710681._000512.pool.root.1"
+        ),
     ),
-    'bphys': sample(
+    "bphys": sample(
         name="ds_bphys",
         rucio_ds="mc16.999031.P8BEG_23lo_ggX18p4_Upsilon1Smumu_4mu_3pt2.deriv.DAOD_BPHY4.e8304_a875_r10724_r10726_p3712_pUM999999",
-        local_path=Path(r"C:\Users\gordo\Code\atlas\data\R21\BPHYS\999031\DAOD_BPHY4.999031._000001.pool.root.1"),
-    )
+        local_path=Path(
+            r"C:\Users\gordo\Code\atlas\data\R21\BPHYS\999031\DAOD_BPHY4.999031._000001.pool.root.1"
+        ),
+    ),
 }
 
+# Base for a typed local xAOD ServiceX dataset
+# (local ==> Will run on a docker container on this machine)
 class xAODLocalTyped(SXLocalxAOD[Event]):
     def __init__(self, file_path: Path):
         super().__init__(file_path, item_type=Event)
@@ -80,23 +70,43 @@ class xAODLocalTyped(SXLocalxAOD[Event]):
 # Make the samples available
 use_local = True
 
+
 def make_ds(s: sample):
+    """Create the a datasample, local or remote, as requested.
+
+    Args:
+        s (sample): The sample to create
+
+    Returns:
+        sample: The sample specification.
+    """
     if use_local:
         ds = xAODLocalTyped(s.local_path)
     else:
         ds = SXDSAtlasxAODR21(s.rucio_ds, backend="dev_xaod")
     return ds
 
-ds_zee = make_ds(_samples['zee'])
-ds_zmumu = make_ds(_samples['zmumu'])
-ds_ztautau = make_ds(_samples['ztautau'])
-ds_jz3_exot15 = make_ds(_samples['jz3_exot15'])
-ds_bphys = make_ds(_samples['bphys'])
+
+# Build the individual samples. First, the DAOD_PHYS samples, which use default
+# calibrations.
+ds_zee = make_ds(_samples["zee"])
+ds_zmumu = make_ds(_samples["zmumu"])
+ds_ztautau = make_ds(_samples["ztautau"])
+ds_bphys = make_ds(_samples["bphys"])
+
+# To demonstrate some features of jets, we need an older R21 sample (which contains
+# topo clusters). Default calibration is different for this guy.
+ds_jz2_exot15 = calib_tools.query_update(
+    make_ds(_samples["jz2_exot15"]),
+    jet_collection="AntiKt4EMTopoJets",
+    jet_calib_truth_collection="AntiKt4TruthJets",
+    perform_overlap_removal=False,
+)
 
 
 ## Some helper methods we'll end up using
 def match_eta_phi(jets, jets_to_match) -> ak.Record:
-    '''Match `jets_to_match` to the `jets` given. There will always be
+    """Match `jets_to_match` to the `jets` given. There will always be
     at least one jet found.
 
     The awkward array needs to have leaves called `pt`, `eta`, and `phi`.
@@ -107,7 +117,7 @@ def match_eta_phi(jets, jets_to_match) -> ak.Record:
 
     Returns:
         _type_: Matched jets 1:1 in `jets` from `jets_to_match`.
-    '''
+    """
 
     to_match_pt = jets_to_match.pt
     to_match_eta = jets_to_match.eta
@@ -122,9 +132,15 @@ def match_eta_phi(jets, jets_to_match) -> ak.Record:
     # TODO: Missing wrap around fro phi
     delta_phi = np.abs(pair_phi[:, :, :]["0"] - pair_phi[:, :, :]["1"])
 
-    delta = delta_eta**2 + delta_phi**2
+    delta = delta_eta ** 2 + delta_phi ** 2
 
     # TODO: remove anything larger that 0.2*0.2
     best_match = ak.argmin(delta, axis=2)
 
-    return ak.Record({"eta": to_match_eta[best_match], "phi": to_match_phi[best_match], "pt": to_match_pt[best_match]})
+    return ak.Record(
+        {
+            "eta": to_match_eta[best_match],
+            "phi": to_match_phi[best_match],
+            "pt": to_match_pt[best_match],
+        }
+    )
