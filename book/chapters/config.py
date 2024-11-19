d97b75eac8bc8617e592a4fcada809a2a240d982
RogerJanusiak <rjanusia@cern.ch>
2024-11-19 01:07:25 -0600

    Updated jets.ipynb first example to use new code

--- book/chapters/config.py
+++ book/chapters/config.py
@@ -1,10 +1,10 @@
 from servicex import deliver, dataset
 
-def get_files(jet_info_per_event):
-
+def get_dataset(jet_info_per_event):
+    """Sends request for dataset to servicex backend. Pass in FuncADLQueryPHYSLITE and returns dictionary of files in dataset."""
     spec = {
         'Sample': [{
-            'Name': "func_adl_xAOD_simple",
+            'Name': "func_adl_xAOD",
             'Dataset': dataset.FileList(
                 [
                     "root://eospublic.cern.ch//eos/opendata/atlas/rucio/mc20_13TeV/DAOD_PHYSLITE.37622528._000013.pool.root.1",  # noqa: E501
