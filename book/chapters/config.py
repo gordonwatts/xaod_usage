024d4ec130ce786dd312ecbf6adb230c7b9b54e8
RogerJanusiak <rjanusia@cern.ch>
2024-11-26 17:01:40 -0600

    Brought back sample structure and updated first example

--- book/chapters/config.py
+++ book/chapters/config.py
@@ -1,12 +1,70 @@
-from servicex import deliver, dataset as servicex_dataset
-import awkward as ak
+from dataclasses import dataclass
+from typing import List, Union
 
-def get_files(query, dataset):
+import awkward as ak
+import numpy as np
+
+from servicex import deliver, dataset as servicex_dataset
+
+@dataclass
+class sample:
+    "Location of data sample"
+    # Shorthand name
+    name: str
+
+    # Full Rucio dataset name
+    ds: Union[str, List[str]]
+
+    # Codegen
+    codegen: str
+
+_samples = {
+    "sx_f": sample(
+        name="sx_f",
+        ds="root://eospublic.cern.ch//eos/opendata/atlas/rucio/mc20_13TeV/DAOD_PHYSLITE.37622528._000013.pool.root.1",
+        codegen="atlasr22"
+    ),
+    "zee_untyped_r21": sample(
+        name="ds_zee_untyped",
+        ds="rucio://mc16_13TeV:mc16_13TeV.361106.PowhegPythia8EvtGen_AZNLOCTEQ6L1_Zee.deriv.DAOD_PHYS.e3601_e5984_s3126_s3136_r10724_r10726_p4164",
+        codegen="atlasr21",
+    ),
+    "zmumu_r21": sample(
+        name="ds_zmuumu",
+        ds="rucio://mc16_13TeV:mc16_13TeV.361107.PowhegPythia8EvtGen_AZNLOCTEQ6L1_Zmumu.deriv.DAOD_PHYS.e3601_e5984_s3126_s3136_r10724_r10726_p4164",
+        codegen="atlasr21",
+    ),
+    "ztautau_r21": sample(
+        name="ds_ztautau",
+        ds="rucio://mc16_13TeV:mc16_13TeV.361108.PowhegPythia8EvtGen_AZNLOCTEQ6L1_Ztautau.deriv.DAOD_PHYS.e3601_e5984_s3126_s3136_r10724_r10726_p4355",
+        codegen="atlasr21",
+    ),
+    "jz2_exot15_r21": sample(
+        name="ds_jz3_exot15",
+        ds="rucio://mc16_13TeV:mc16_13TeV.361022.Pythia8EvtGen_A14NNPDF23LO_jetjet_JZ2W.deriv.DAOD_EXOT15.e3668_s3126_r9364_r9315_p4696",
+        codegen="atlasr21",
+    ),
+    "bphys_r21": sample(
+        name="ds_bphys",
+        ds=["root://eosatlas.cern.ch//eos/atlas/user/d/daits/mc16_13TeV/DAOD_BPHY4/mc16.999031.P8BEG_23lo_ggX18p4_Upsilon1Smumu_4mu_3pt2.deriv.DAOD_BPHY4.e8304_a875_r10724_r10726_p3712_pUM999999/DAOD_BPHY4.999031._000001.pool.root.1"],
+        codegen="atlasr21",
+    ),
+    "ttbar_r22": sample(
+        name="ds_ttbar",
+        ds=[],
+        codegen="atlasr21",
+    )
+}
+
+# sx_f means servicex-frontend documentation dataset, need to find a better name for this, will update after I learn how to find ds names
+sx_f = _samples["sx_f"]
+
+def get_files(query, s: sample):
     """Sends request for data to servicex backend.
     
     Args:
         query: FuncADLQueryPHYSLITE
-        dataset: String of dataset location
+        s: sample class with dataset to call from
 
     Returns:
         List of files returned from servicex backend
@@ -14,32 +72,17 @@ def get_files(query, dataset):
     """
     spec = {
         'Sample': [{
-            'Name': "func_adl_xAOD",
+            'Name': s.name,
             'Dataset': servicex_dataset.FileList(
-                [ dataset ]
+                [ s.ds ]
             ),
             'Query': query,
-            'Codegen': 'atlasr22',
+            'Codegen': s.codegen,
         }]
     }
-    files = deliver(spec, servicex_name="atlasr22")['func_adl_xAOD']
+    files = deliver(spec, servicex_name="atlasr22")[s.name]
     assert files is not None, "No files returned from deliver! Internal error"
     return files
-
-
-def get_files_physlite(query):
-    """Sends request for data to servicex backend using a specific PHYSLITE dataset.
-    
-    Args:
-
-        query: FuncADLQueryPHYSLITE
-
-    Returns:
-        List of files returned from servicex backend
-    
-    """
-    dataset = "root://eospublic.cern.ch//eos/opendata/atlas/rucio/mc20_13TeV/DAOD_PHYSLITE.37622528._000013.pool.root.1"
-    return get_files(query,dataset)
 
 
 def match_eta_phi(jets, jets_to_match) -> ak.Record:
